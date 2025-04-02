c5b42acf1fa3d15429c99a15258e0d7ca366f469
Jason Alexander Quinn <149098839+MindisBlank@users.noreply.github.com>
2025-04-02 20:40:08 +0200

    Update main4FA.py to use Nordic32_SA.txt and adjust fault parameters for analysis

--- main4FA.py
+++ main4FA.py
@@ -3,9 +3,9 @@
 Main Script for Fault Analysis
 """
 
-#import FaultAnalysis_46705 as fa    # import Fault Analysis functions
+import FaultAnalysis_46705 as fa    # import Fault Analysis functions
 import LoadNetworkData4FA as lnd4fa # load the network data to global variables
-filename = "TestSystem.txt" # TestSystem.txt  Nordic32_SA.txt
+filename = "Nordic32_SA.txt" # TestSystem.txt  Nordic32_SA.txt
 lnd4fa.LoadNetworkData4FA(filename) # makes Zbus0 available as lndfa.Zbus0 etc.
 print('**********Start of Fault Analysis**********')
 
@@ -19,16 +19,16 @@ print('**********Start of Fault Analysis**********')
 
 
 # Carry out the fault analysis ... 
-FaultBus = 3
+FaultBus = 4011
 # FaultType: 0 = 3-phase balanced fault; 1 = Single Line-to-Ground fault;
 #            2 = Line-to-Line fault;     3 = Double Line-to-Ground fault.
-FaultType = 1
+FaultType = 0
 FaultImpedance = 0 # (in pu) 
 PrefaultVoltage = 1.000 # (in pu)
 # Iph: phase current array (0: phase a; 1: phase b; 2: phase c). 
 # Vph_mat: phase line-to-ground voltages (rows: busses; columns: phases a, b, c).
-#Iph,Vph_mat = fa.FaultAnalysis(lnd4fa.Zbus0,lnd4fa.Zbus1,lnd4fa.Zbus2,lnd4fa.bus_to_ind, 
-#                                FaultBus,FaultType,FaultImpedance,PrefaultVoltage)
+Iph,Vph_mat = fa.FaultAnalysis(lnd4fa.Zbus0,lnd4fa.Zbus1,lnd4fa.Zbus2,lnd4fa.bus_to_ind, 
+                               FaultBus,FaultType,FaultImpedance,PrefaultVoltage)
 # Display results
-#fa.DisplayFaultAnalysisResults(Iph,Vph_mat,FaultBus,FaultType,FaultImpedance,PrefaultVoltage)
-#print('**********End of Fault Analysis**********')
+fa.DisplayFaultAnalysisResults(Iph,Vph_mat,FaultBus,FaultType,FaultImpedance,PrefaultVoltage)
+print('**********End of Fault Analysis**********')
