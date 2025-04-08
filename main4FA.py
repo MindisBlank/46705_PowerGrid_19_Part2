b5dd50bdd4221466cae48b09b066e1ffc1c6d036
Jason Alexander Quinn <149098839+MindisBlank@users.noreply.github.com>
2025-04-08 12:21:01 +0200

    Update filename in main4FA.py to Nordic32_4FA.txt and change FaultType to Single Line-to-Ground fault

--- main4FA.py
+++ main4FA.py
@@ -5,7 +5,7 @@ Main Script for Fault Analysis
 import numpy as np  # Ensure NumPy is imported
 import FaultAnalysis_46705 as fa    # import Fault Analysis functions
 import LoadNetworkData4FA as lnd4fa # load the network data to global variables
-filename = "Nordic32_SA.txt" # TestSystem4FA.txt  Nordic32_SA.txt
+filename = "Nordic32_4FA.txt" # TestSystem4FA.txt  Nordic32_SA.txt
 lnd4fa.LoadNetworkData4FA(filename) # makes Zbus0 available as lndfa.Zbus0 etc.
 
 print('**********Start of Fault Analysis**********')
@@ -24,7 +24,7 @@ else :
     FaultBus = 4011
     # FaultType: 0 = 3-phase balanced fault; 1 = Single Line-to-Ground fault;
     #            2 = Line-to-Line fault;     3 = Double Line-to-Ground fault.
-    FaultType = 3
+    FaultType = 1
     FaultImpedance = 0 # (in pu) 
     PrefaultVoltage = 1.000 # (in pu)
     # Iph: phase current array (0: phase a; 1: phase b; 2: phase c). 
